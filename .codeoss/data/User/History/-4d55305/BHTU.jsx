28dde436476cc6bd0f67613fbc04b42d8122becc
Rajesh P <penurajeshitsd@gmail.com>
2024-11-14 13:02:06 +0000

    // Variation 2: step:2 Spread and Rest Operators // Final Step

---
+++ .codeoss/data/User/History/-4d55305/BHTU.jsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search search={searchTerm} onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const Search = ({ onSearch, search }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input value={search} id="search" type="text" onChange={onSearch} />
+  </div>
+);
+
+// Variation 2: step:2 Spread and Rest Operators
+// Final Step
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} fine="rajesh" love="rajh_" {...item} />
+    ))}
+  </ul>
+);
+
+const Item = ({
+  url, title, author, num_comments, points, love ,fine }
+) => (
+  <li>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+    <span>{love}{fine}</span> {/* Display the custom property if needed */}
+  </li>
+);
+
+
+export default App;
